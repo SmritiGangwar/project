51567c53a2127cb807d7c9915ebace5a2411603e
smriti <smritigangwark9@gmail.com>
2021-11-11 21:09:17 +0530

    Initial commit

---
+++ src/Test1.java
@@ -0,0 +1,16 @@
+public class Test1 {
+    public static void draw(){
+            System.out.println("Drawing Shape");
+        }
+        public static void erase(){
+
+            System.out.println("Erasing Shape");
+        }
+        public static void main(String[] args) {
+            draw();
+            erase();
+        }
+
+    }
+
+

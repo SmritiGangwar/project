efdf4a978974a34bd6275a9906e4f0a23f9c8c84
smriti <smritigangwark9@gmail.com>
2021-11-18 20:41:55 +0530

    Initial commit

---
+++ src/PalindromeArray.java
@@ -0,0 +1,19 @@
+public class PalindromeArray {
+    static void palindrome(int arr[], int n){
+        int flag = 0;
+        for (int i=0;i<= n/2 && n!=0;i++) {
+            if (arr[i] != arr[n-i-1]){
+                flag=1;
+                break;
+            }
+        }
+        if (flag==1)
+            System.out.println("Not Palindrome");
+        else
+            System.out.println("Palindrome");
+    }
+    public static void main(String[] args) {
+        int arr[] = { 1,3,6,5,3,2};
+        int n = arr.length;
+        palindrome(arr, n);
+    }

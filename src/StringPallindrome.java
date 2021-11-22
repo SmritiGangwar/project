244b023b5b9286f6eae48806045440a0eadca0ef
smriti <smritigangwark9@gmail.com>
2021-11-22 23:27:12 +0530

    Initial commit

---
+++ src/StringPallindrome.java
@@ -0,0 +1,20 @@
+public class StringPallindrome {
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public static void main(String[] args)
+    {
+        String str = "jahaj";
+        if (isPalindrome(str))
+            System.out.print("Yes");
+        else
+            System.out.print("No");
+    }
+}

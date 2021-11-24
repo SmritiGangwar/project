430f2d21c791bf39e7b9bf846746dd223c4da9c3
smriti <smritigangwark9@gmail.com>
2021-11-24 23:07:03 +0530

    Initial commit

---
+++ src/StringCommLineArgs.java
@@ -0,0 +1,6 @@
+public class StringCommLineArgs{
+    public static void main(String args[]) {
+        System.out.println(args[0] + " University " + args[1]);
+        System.out.println(args[0] + "Technologies" + args[1]);
+    }
+}

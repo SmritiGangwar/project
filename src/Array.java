26b8f5f8435b97e0c315fc138f35a6dbf765eb88
smriti <smritigangwark9@gmail.com>
2021-11-29 21:21:54 +0530

    Initial commit

---
+++ src/Array.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        try{
+            int a = s.nextInt();
+            int b = s.nextInt();
+            int arr[]={12,34,56,78,90};
+            int index = s.nextInt();
+
+            System.out.println(a/b);
+            System.out.println(arr[index]);
+        }
+        catch(ArrayIndexOutOfBoundsException obj1)
+        {
+            System.out.println(obj1);
+        }
+        catch(ArithmeticException obj1)
+        {
+            System.out.println(obj1);
+        }
+        catch(Exception obj1)
+        {
+            System.out.println(obj1);
+        }
+    }
+}
